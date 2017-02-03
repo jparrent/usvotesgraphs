0dfb19a08cfbbea2277f1719aa553adc5700969a
jparrent <=>
2017-02-02 21:15:30 -0500

    Updated plot_2D_tSNE in build_features.py and uploaded two exploratory figures.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -14,6 +14,8 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import TSNE
+from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import StandardScaler, RobustScaler
 import matplotlib.pyplot as plt
 
 
@@ -32,76 +34,142 @@ class Features:
         """Read input dataframe.csv files (Votes Records) in
         ../../data/processed/ and return Party (y_labels) and votes cast (X_data)
         """
-        # // TODO pass label variable to select between metadata labels
 
         df = self._data.set_index('Name')
-        cols = df.columns.tolist()[2:]
-        # metadata = self._data[['Name', 'Party', 'State']].values
-        names = df.index.values
-        y_labels = df['Party'].values
-        X_data = df[cols].values
-        return names, y_labels, X_data
+        return df
 
-    def plot_2D_tSNE(self, names, labels, data, congressman=[], n_features_SVD=50):
+    def transform_SVD_tSNE(self, df, chamber, n_features_SVD=50,
+                           n_components=2, scale='standard'):
         """Transform 1800+ features (measures/bills) to 50 features using
         truncated singular value decomposition (SVD). This is followed by
-        creating a t-distributed stochastic neighbor embedding (t-SNE), and
-        plotting the resulting 2D array.
+        creating and returning a t-distributed stochastic neighbor embedding
+        (t-SNE).
         """
-        # // TODO pass n_features_SVD and labels logic and color mapping
+
+        df = df[df.Chamber == chamber]
+        cols = df.columns.tolist()[3:]
 
         svd = TruncatedSVD(n_features_SVD)
-        X_trunc = svd.fit_transform(data)
+        svd_mapper = DataFrameMapper([(df.columns[3:], svd)])
+        X_trunc = svd_mapper.fit_transform(df.copy())
 
-        t_SNE = TSNE(n_components=2, random_state=0)
+        tSNE = TSNE(n_components=n_components, random_state=0)
         np.set_printoptions(suppress=True)
-        X_tSNE = t_SNE.fit_transform(X_trunc)
+        X_tSNE = tSNE.fit_transform(X_trunc)
 
-        dems_idx = [i for i, v in enumerate(labels) if v == 'D']
-        vis_x_dems = [X_tSNE[i, 0] for i in dems_idx]
-        vis_y_dems = [X_tSNE[i, 1] for i in dems_idx]
+        if scale != 'robust':
 
-        reps_idx = [i for i, v in enumerate(labels) if v == 'R']
-        vis_x_reps = [X_tSNE[i, 0] for i in reps_idx]
-        vis_y_reps = [X_tSNE[i, 1] for i in reps_idx]
+            X_tSNE = StandardScaler().fit_transform(X_tSNE)
 
-        inds_idx = [i for i, v in enumerate(labels) if v == 'I']
-        vis_x_inds = [X_tSNE[i, 0] for i in inds_idx]
-        vis_y_inds = [X_tSNE[i, 1] for i in inds_idx]
+        X_tSNE = RobustScaler().fit_transform(X_tSNE)
 
-        colors = ['b', 'r', 'm']
+        df_X_tSNE = pd.DataFrame(X_tSNE, index=df.index)
+        df_tSNE = pd.concat([df[['Party', 'State']], df_X_tSNE], axis=1)
+
+        return df_tSNE
+
+    def plot_2D_tSNE(self, df_senate, df_house, senators=[], representatives=[]):
+        """Plot tSNE for senate and house.
+        """
+
+        f, (senate, house) = plt.subplots(2, sharex=False, sharey=False)
+        marker = '.'
+        alpha = 0.7
         legend_labels = ['Dem', 'Reb', 'Ind']
+        colors = ['b', 'r', 'm']
+        senate_markers = ['*', '+', 'x', '^']
+        house_markers = ['v', '<', '>', 'D']
 
-        Dems = plt.scatter(vis_x_dems, vis_y_dems, c=colors[0], alpha=0.5)
-        Reps = plt.scatter(vis_x_reps, vis_y_reps, c=colors[1], alpha=0.5)
-        Inds = plt.scatter(vis_x_inds, vis_y_inds, c=colors[2], alpha=0.5)
+        """Senate
+        """
+        df_dems_senate = df_senate[(df_senate.Party == 'D')]
+        vis_x_dems_senate = df_dems_senate[0].values
+        vis_y_dems_senate = df_dems_senate[1].values
+
+        df_reps_senate = df_senate[(df_senate.Party == 'R')]
+        vis_x_reps_senate = df_reps_senate[0].values
+        vis_y_reps_senate = df_reps_senate[1].values
+
+        df_inds_senate = df_senate[(df_senate.Party == 'I')]
+        vis_x_inds_senate = df_inds_senate[0].values
+        vis_y_inds_senate = df_inds_senate[1].values
+
+        """House
+        """
+        df_dems_house = df_house[(df_house.Party == 'D')]
+        vis_x_dems_house = df_dems_house[0].values
+        vis_y_dems_house = df_dems_house[1].values
+
+        df_reps_house = df_house[(df_house.Party == 'R')]
+        vis_x_reps_house = df_reps_house[0].values
+        vis_y_reps_house = df_reps_house[1].values
+
+        df_inds_house = df_house[(df_house.Party == 'I')]
+        vis_x_inds_house = df_inds_house[0].values
+        vis_y_inds_house = df_inds_house[1].values
+
+        """Senate Scatter Plot
+        """
+        Dems = senate.scatter(vis_x_dems_senate, vis_y_dems_senate, c=colors[0],
+                              marker=marker, alpha=alpha)
+        Reps = senate.scatter(vis_x_reps_senate, vis_y_reps_senate, c=colors[1],
+                              marker=marker, alpha=alpha)
+        Inds = senate.scatter(vis_x_inds_senate, vis_y_inds_senate, c=colors[2],
+                              marker=marker, alpha=alpha)
+        senate.set_title('t-SNE of US Senate Votes')
+
+        """House Scatter Plot
+        """
+        house.scatter(vis_x_dems_house, vis_y_dems_house, c=colors[0],
+                      marker=marker, alpha=alpha)
+        house.scatter(vis_x_reps_house, vis_y_reps_house, c=colors[1],
+                      marker=marker, alpha=alpha)
+        house.scatter(vis_x_inds_house, vis_y_inds_house, c=colors[2],
+                      marker=marker, alpha=alpha)
+        house.set_title('t-SNE of US House Votes')
+
+        f.subplots_adjust(hspace=0.3)
+        senate.axes.get_xaxis().set_ticks([])
+        senate.axes.get_yaxis().set_ticks([])
+        house.axes.get_xaxis().set_ticks([])
+        house.axes.get_yaxis().set_ticks([])
+        senate.axis('off')
+        house.axis('off')
 
         legend_groups = [Dems, Reps, Inds]
 
-        if congressman:
+        if senators:
 
-            markers = ['*', '+', 'x', '^', 'v', '<', '>', 'D']
+            for p, person in enumerate(senators):
 
-            for p, person in enumerate(congressman):
-
-                idx = [i for i, v in enumerate(names) if v.lower() == person.lower()]
-                vis_x = [X_tSNE[i, 0] for i in idx]
-                vis_y = [X_tSNE[i, 1] for i in idx]
-
-                point = plt.scatter(vis_x, vis_y, s=75, marker=markers[p], c='black')
-                legend_labels.append(person)
+                vis_x, vis_y = df_senate[(df_senate.index == person)].values[0, 2:]
+                point = senate.scatter(vis_x, vis_y, s=75,
+                                       marker=senate_markers[p], c='black')
                 legend_groups.append(point)
+                legend_labels.append(person)
 
-        plt.legend(legend_groups,
-                   legend_labels,
-                   scatterpoints=1,
-                   loc='upper center',
-                   bbox_to_anchor=(0.5, 1.14),
-                   fancybox=True,
-                   shadow=True,
-                   borderaxespad=0.,
-                   ncol=5,
-                   fontsize=10)
+        if representatives:
+
+            for p, person in enumerate(representatives):
+
+                vis_x, vis_y = df_house[(df_house.index == person)].values[0, 2:]
+                point = house.scatter(vis_x, vis_y, s=70,
+                                      marker=house_markers[p], c='black')
+                legend_groups.append(point)
+                legend_labels.append(person)
+
+        f.legend(legend_groups,
+                 legend_labels,
+                 scatterpoints=1,
+                 loc='upper left',
+                 prop={'size': 10},
+                 labelspacing=0.5,
+                 bbox_to_anchor=(0.01, 0.7),
+                 fancybox=True,
+                 shadow=True,
+                 borderaxespad=0.,
+                 ncol=1,
+                 fontsize=10)
 
         plt.show()
 
@@ -114,16 +182,24 @@ def main(session_number):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-    congressional_features = Features(session_number)
-    names, y_labels, X_data = congressional_features.load()
-    congressmen = ['Sanders',
-                   'Reid',
-                   'McConnell',
-                   'Boehner',
-                   'Pelosi',
-                   'Cantor',
-                   'Wasserman']
-    congressional_features.plot_2D_tSNE(names, y_labels, X_data, congressmen)
+    congressional_votes = Features(session_number)
+    df = congressional_votes.load()
+    senators = ['Sanders',
+                'Reid',
+                'McConnell',
+                ]
+    representatives = ['Boehner',
+                       'Pelosi',
+                       'Cantor',
+                       'Wasserman'
+                       ]
+    scale = 'robust'
+
+    df_tSNE_senate = congressional_votes.transform_SVD_tSNE(df, chamber='s', scale=scale)
+    df_tSNE_house = congressional_votes.transform_SVD_tSNE(df, chamber='h', scale=scale)
+
+    congressional_votes.plot_2D_tSNE(df_tSNE_senate, df_tSNE_house,
+                                     senators, representatives)
 
 
 if __name__ == '__main__':
